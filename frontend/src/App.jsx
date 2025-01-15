318586fe62646f7450543fbfb3634b90d7b05132
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-01-16 02:49:20 +0530

    Fixed app.css import issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CurrencyConverter from "./components/CurrencyConverter";
-import './app.css'
+import './App.css'
 import Navbar from "./components/Navbar";
 
 function App(){
