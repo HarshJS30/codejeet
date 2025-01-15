73ed3ff6ee84bcb828705f2d3836bfdd169cc2a1
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-01-16 03:34:10 +0530

    commit

--- frontend/src/apis/Currencyapi.js
+++ frontend/src/apis/Currencyapi.js
@@ -1,22 +1,21 @@
-const API_KEY = 'e3b7f6179b69f6c206614216';
-
 export const CurrencyConversion = async (fromCurrency, toCurrency, amount) => {
-    const response = await fetch(`https://api.exchangerate-api.com/v6/${API_KEY}/pair/${fromCurrency}/${toCurrency}/${amount}`);
+    const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`);
     const data = await response.json();
     
-    if (data.result === "success") {
-        return data.conversion_result;
+    if (data.rates) {
+        return data.rates[toCurrency];
     } else {
         throw new Error('Something up with the backend!');
     }
 }
 
 export const getCurrencyCodes = async () => {
-    const response = await fetch(`https://api.exchangerate-api.com/v6/${API_KEY}/codes`);
+    const response = await fetch('https://api.frankfurter.app/currencies');
     const data = await response.json();
 
-    if (data.result === "success") {
-        return data.supported_codes.map(([code, name]) => ({ code, name }));
+    if (data) {
+        // Convert object to array of {code, name} objects
+        return Object.entries(data).map(([code, name]) => ({ code, name }));
     } else {
         throw new Error('Failed to fetch currency codes');
     }
